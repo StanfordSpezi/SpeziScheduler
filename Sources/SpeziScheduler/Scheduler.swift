2e6017a6fbfef8ae6be0a4f4dd44e02ef23e9cd3
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2023-05-12 01:24:07 -0700

    Updates the Project to be Part of the Spezi Organization (#6)

--- Sources/SpeziScheduler/Scheduler.swift
+++ Sources/SpeziScheduler/Scheduler.swift
@@ -1,14 +1,14 @@
 //
-// This source file is part of the CardinalKit open-source project
+// This source file is part of the Stanford Spezi open-source project
 //
 // SPDX-FileCopyrightText: 2022 Stanford University and the project authors (see CONTRIBUTORS.md)
 //
 // SPDX-License-Identifier: MIT
 //
 
-import CardinalKit
 import Combine
 import Foundation
+import Spezi
 
 
 /// The ``Scheduler/Scheduler`` module allows the scheduling and observation of ``Task``s adhering to a specific ``Schedule``.
