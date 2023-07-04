0d02120b086625f5cc12e29b94f3b14bb76a64d0
Philipp Zagar <philipp.zagar@gmail.com>
2023-07-04 12:39:28 +0200

    Incorporate review

--- Sources/SpeziScheduler/Scheduler.swift
+++ Sources/SpeziScheduler/Scheduler.swift
@@ -77,6 +77,8 @@ public class Scheduler<ComponentStandard: Standard, Context: Codable>: NSObject,
     public func requestLocalNotificationAuthorization() async throws {
         if await !localNotificationAuthorization {
             try await UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound])
+            // Triggers an update of the UI in case the notification permissions are changed
+            self.objectWillChange.send()
         }
         
         updateScheduleTaskAndNotifications()
