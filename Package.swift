d2950c87a9f67c92c904b0ba86c179decd3144e4
Andreas Bauer <andreas.bauer@stanford.edu>
2024-09-17 15:38:38 +0200

    Current progress towards smarter scheduling for tasks

--- Package.swift
+++ Package.swift
@@ -27,11 +27,11 @@ let package = Package(
         .library(name: "SpeziSchedulerUI", targets: ["SpeziSchedulerUI"])
     ],
     dependencies: [
-        .package(url: "https://github.com/StanfordSpezi/SpeziFoundation", from: "2.0.0-beta.2"),
-        .package(url: "https://github.com/StanfordSpezi/Spezi", branch: "feature/application-for-swiftui"),
-        .package(url: "https://github.com/StanfordSpezi/SpeziViews", branch: "feature/additional-infrastructure"),
-        .package(url: "https://github.com/StanfordSpezi/SpeziStorage", from: "1.1.2"),
-        .package(url: "https://github.com/swiftlang/swift-syntax", from: "600.0.0-prerelease-2024-08-14"),
+        .package(url: "https://github.com/StanfordSpezi/SpeziFoundation.git", from: "2.0.0-beta.2"),
+        .package(url: "https://github.com/StanfordSpezi/Spezi.git", branch: "feature/application-for-swiftui"),
+        .package(url: "https://github.com/StanfordSpezi/SpeziViews.git", branch: "feature/additional-infrastructure"),
+        .package(url: "https://github.com/StanfordSpezi/SpeziStorage.git", from: "1.1.2"),
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0-prerelease-2024-08-14"),
         .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.17.2")
     ] + swiftLintPackage(),
     targets: [
