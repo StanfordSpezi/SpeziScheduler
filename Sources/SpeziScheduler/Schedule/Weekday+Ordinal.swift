c1a3a3dc255ab99762b99fbccc50a656e464984b
Andreas Bauer <andreas.bauer@stanford.edu>
2024-12-15 22:18:17 +0100

    Locale.Weekday extensions

--- Sources/SpeziScheduler/Schedule/Weekday+Ordinal.swift
+++ Sources/SpeziScheduler/Schedule/Weekday+Ordinal.swift
@@ -10,7 +10,8 @@ import Foundation
 
 
 extension Locale.Weekday {
-    var ordinal: Int {
+    /// Retrieve the ordinal of the weekday.
+    public var ordinal: Int {
         switch self {
         case .sunday:
             1
@@ -29,5 +30,38 @@ extension Locale.Weekday {
         @unknown default:
             preconditionFailure("A new weekday appeared we don't know about: \(self)")
         }
+    }
+    
+    /// Initialize the weekday from it's ordinal representation.
+    /// - Parameter ordinal: The ordinal. A value between 1 and 7.
+    public init?(ordinal: Int) {
+        switch ordinal {
+        case 1:
+            self = .sunday
+        case 2:
+            self = .monday
+        case 3:
+            self = .tuesday
+        case 4:
+            self = .wednesday
+        case 5:
+            self = .thursday
+        case 6:
+            self = .friday
+        case 7:
+            self = .saturday
+        default:
+            return nil
+        }
+    }
+    
+    /// Get the weekday from a date.
+    /// - Parameter date: The date.
+    public init(from date: Date) {
+        let weekdayOrdinal = Calendar.current.component(.weekday, from: date)
+        guard let weekday = Locale.Weekday(ordinal: weekdayOrdinal) else {
+            preconditionFailure("Failed to derive weekday from ordinal \(weekdayOrdinal)")
+        }
+        self = weekday
     }
 }
