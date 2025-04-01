8e3f5fdaf37fee24cde0089f98fdedf308af0013
Lukas Kollmer <hey@lukaskollmer.de>
2025-04-01 21:31:14 +0200

    task context: configure JSONEncoder to encode fields in order
    this should reduce (though not eliminate) the risk of unnecessarily creating new task versions as a result of context equality comparisons returning false negatives (we're comparing the encoded Data, rather than the actual values, and since there are multiple encodings for a value, equal values might still compare false...)

--- Tests/SpeziSchedulerTests/Utils/ExampleTaskKey.swift
+++ Tests/SpeziSchedulerTests/Utils/ExampleTaskKey.swift
@@ -9,6 +9,21 @@
 import SpeziScheduler
 
 
+struct NonTrivialTaskContext: Codable {
+    // give it a bunch of fields to maximise the likelihood of something being out of order
+    let field0: Int
+    let field1: Int
+    let field2: Int
+    let field3: Int
+    let field4: Int
+    let field5: Int
+    let field6: Int
+    let field7: Int
+    let field8: Int
+    let field9: Int
+}
+
+
 extension Outcome {
     @Property var example: String?
 }
@@ -21,4 +36,8 @@ extension Task.Context {
 
 extension Task.Context {
     @Property var example2: String = "Hello World"
+}
+
+extension Task.Context {
+    @Property(coding: .json) var nonTrivialExample: NonTrivialTaskContext?
 }
